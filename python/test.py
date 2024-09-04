2e6104528381f164f66c52ed83b61f69fea5ddf7
uwohlen <112326477+uwohlen@users.noreply.github.com>
2024-09-04 10:50:04 +0200

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,2 +1,28 @@
-print("hei")
-print("ha det")
+fortsett = True
+
+while fortsett:
+  svar = input("Skriv inn et heltall: ")
+  try:
+    tall = int(svar)
+    fortsett = False
+  except:
+    print("Du må faktisk skrive et heltall!")
+
+print("Løkke ferdig.")
+print(tall)
+
+
+meny = True
+while meny:
+  print()
+  print("Meny")
+  print("A - riktig valg")
+  print("B - ikke et godt valg")
+  svar = input("Velg: ")
+  if (svar == "A"):
+    print("wow")
+    meny = False
+  elif (svar == "B"):
+    print("fortsett")
+  else:
+    print("invalid, prøv igjen")
