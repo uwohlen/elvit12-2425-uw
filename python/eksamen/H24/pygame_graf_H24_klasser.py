df1c03af7d50d234679f8c7f8d81bb929beb3c52
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-03-28 16:22:47 +0100

    Update pygame_graf_H24_klasser.py

--- python/eksamen/H24/pygame_graf_H24_klasser.py
+++ python/eksamen/H24/pygame_graf_H24_klasser.py
@@ -1,13 +1,42 @@
 import pygame as pg
 
 class Firkant:
-  def __init__(self,vindu,x,y,bredde,hoyde,tekst,farge):
+  def __init__(self,vindu,x,y,bredde,hoyde,farge):
     self.vindu = vindu
     self.x = x
     self.y = y
     self.bredde = bredde
     self.hoyde = hoyde
-    self.tekst = tekst
-    self.obj = pg.Rect
+    self.farge = farge
+    self.obj = pg.Rect(self.x, self.y, self.bredde, self.hoyde)
 
+  def tegn(self):
+    pg.draw.rect(self.vindu, self.farge, self.obj)
+
+
+class Knapp(Firkant):
+  def __init__(self,vindu,x,y,bredde,hoyde,farge,tekst):
+    super().__init__(vindu,x,y,bredde,hoyde,farge)
+    self.tekst = tekst
+
+  def vis_tekst(self):
+    pass
+
+  def klikk(self):
+    pass
+
+
+class Nedtrekk(Knapp):
+  def __init__(self,vindu,x,y,bredde,hoyde,farge,tekst,alternativer):
+    super().__init__(vindu,x,y,bredde,hoyde,farge,tekst)
+    self.alternativer = alternativer
+    self.alt_obj = [] 
+  
+  def lag_alt_obj(self):
+    # lag liste med objekter, ett objekt for hvert alternativ
+    pass
+
+  def klikk(self):
+    # vis alternativer
+    pass
 
