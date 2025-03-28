88344c6816a72aadecab5701a48781634afd929c
uwohlen <112326477+uwohlen@users.noreply.github.com>
2025-03-28 15:31:04 +0100

    klasser!

---
+++ python/eksamen/H24/pygame_graf_H24_klasser.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+class Firkant:
+  def __init__(self,vindu,x,y,bredde,hoyde,tekst,farge):
+    self.vindu = vindu
+    self.x = x
+    self.y = y
+    self.bredde = bredde
+    self.hoyde = hoyde
+    self.tekst = tekst
+    self.obj = pg.Rect
+
+
